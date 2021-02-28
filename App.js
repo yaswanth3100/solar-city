5ee2449b35c7f86b733690f9b1deef88d9fa2c31
yaswanth3100 <46052231+yaswanth3100@users.noreply.github.com>
2021-02-28 10:32:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,156 @@
+import './App.css';
+import { Button, TextField,FormControl} from '@material-ui/core'
+import React from 'react';
+import data from "./carousel"
+function App() {
+  const [index,setIndex]=React.useState(0);
+  const myref=React.useRef(null);
+  const scrollToRef = (ref) =>{
+    // window.scrollTo(0, ref.current.offsetTop) 
+     myref.current?.scrollIntoView({ behavior: 'smooth' });
+  } 
+const handleLeftClick=()=>{
+  if(index>0)
+  {
+    setIndex(index-1);
+  }
+  else{
+    setIndex(data.length-1);
+  }
+
+}
+const handleRightClick=()=>{
+  if(index<data.length-1)
+  {
+    setIndex(index+1);
+  }
+  else{
+    setIndex(0);
+  }
+}
+  return (
+    <div className="main">
+      <div className="starter">
+        <nav className="nav-bar">
+          <div className="logo">
+            <h2>Unicorn</h2>
+          </div>
+
+        
+          <div className="nav-links">
+            <div>
+              <a className="call" onClick={()=>scrollToRef(myref)} >Contact Us</a>
+            </div>
+          </div>
+        </nav>
+        <div className="line">
+        
+        </div>
+        <div className="starter__texts">
+          <h1>
+            let the magic begin..with silicon and sunshine!
+          </h1>
+          <p>
+            G2V is a team of Energy Engineers specializing in powering your building with Clean and Uninterrupted power.​
+          </p>
+          <img  className="scrolldown" src="https://media4.giphy.com/media/Wtg8Bmgul1Qxc0otod/200.webp?cid=ecf05e4714g8whrrd2wb4reagcolstxurt1vvzn9lu3hdhnm&rid=200.webp" alt=""  />
+        </div>
+      </div>
+      <div className="line-end"></div>
+      <div className="features">
+        <div className="uniques">
+          <div>
+            <img src="https://media1.giphy.com/media/LPZtw1JynGuTIEcDVS/200w.webp?cid=ecf05e47xs7otlg43rhaqs01u4ppmk0nbmmervgesbhejje5&rid=200w.webp" alt=""/>
+          </div>
+          <div className="uniques-content">
+            <h1>Excellence in Design and Engineering</h1>
+            <p>
+              We don't just set up Solar for your Rooftop.
+              We solve your Energy Problems.
+            </p>
+          </div>
+        </div>
+
+        <div className="uniques2">
+          <div className="uniques-content2">
+            <h1>
+            Best utilisation of your Sunny Rooftop</h1>
+            <p>
+              Flat roof? Sloped roof? Sheet metal shed?
+              No worries. We got you covered.
+            </p>
+          </div>
+          <div>
+            <img src="https://media3.giphy.com/media/XGImpWsgw3VIQEbXQ3/200w.webp?cid=ecf05e47b7iqtw4wsgh41hmvtrpxyoigdziqdrvntcbsi8mj&rid=200w.webp" alt=""/>
+          </div>
+        </div>
+
+         <div className="uniques">
+          <div>
+            <img src="https://media2.giphy.com/media/PjaVO2QgB95iKsRmGZ/200w.webp?cid=ecf05e472bg4oqr0b6ws2eklfl5pvag65jmch89mdrw9qigu&rid=200w.webp" alt=""/>
+          </div>
+          <div className="uniques-content">
+            <h1>One size doesn't fit all..</h1>
+            <p>
+              You are Unique. So is your Energy Consumption Pattern.
+We customize your solution. Just For You.
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+{/* <div className="line-end"></div> */}
+<div className="caption">
+  <h1>Re-powering Your Abode</h1>
+    <h3>We offer a range of cost-effective solutions which do not necessitate any reduction in your energy usage while bringing down your usage bills substantially.</h3>
+</div>
+      <div ref={myref} className="contact">
+        <form action="" className="forms">
+
+         <center> <h1>Contact us</h1></center>
+         <div className="each-field">
+          <TextField className="outlined-basic" size="large" id="outlined-basic"  variant="filled" label="Name" placeholder="Name"/>
+         </div>
+        <div className="each-field">
+          <TextField id="outlined-basic"  variant="filled" label="Town/City" placeholder="Town/City" size="large" />
+        </div>
+        <div className="each-field">
+          <TextField className="outlined-basic" size="large" id="outlined-basic"  variant="filled" label="email" placeholder="Email"/>
+         </div>
+        <div className="each-field">
+          <TextField id="outlined-basic"  variant="filled" label="Phone no:" placeholder="Phone no "/>
+        </div>
+        <div>
+        <Button variant="outlined" color="primary" size="medium">Submit</Button>
+        </div>
+        
+        </form>
+        <div className="gallery">
+          <div>
+            <button onClick={handleLeftClick}><i class="fas fa-angle-left"></i></button>
+          </div>
+          <div>
+            <img className="caro-img" src={data[index].image} alt=""/>
+          </div>
+          <div className="caro-texts">
+            <h1>{data[index].title}</h1>
+            <h3>{data[index].description}</h3>
+          </div>
+          <div>
+            <button onClick={handleRightClick}><i class="fas fa-angle-right"></i></button>
+          </div>
+        </div>
+      </div>
+      <footer className="foot">
+        <h3>G2V Solar</h3>
+        <p>inc@g2vsolar.com</p>
+        <p>+91 93846 11104 | +91 93846 11106</p>
+        <p>G2V Solar Solutions Pvt. Ltd</p>
+        <p>Visakhapatnam</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ carousel.js
@@ -0,0 +1,20 @@
+const data=[
+ {
+  title:"Uninterrupted Power",
+  description:"We design and install systems that mitigate the issues of power outages, grid instabilities, absence or unfeasibility of grid power so that you enjoy a seamless and surge-less power supply",
+  image:"https://static.wixstatic.com/media/48348e_44dc9618db01420bb0fddb049a1ea286~mv2.jpg/v1/fill/w_462,h_556,al_c,lg_1,q_80/WhatsApp%2520Image%25202020-08-16%2520at%252019_.webp"
+ },
+  {
+  title:"Autonomous Solar Pumpsets",
+  description:`Unmonitored overflow of water from storage tanks? Inflated power bills from extended usage of high-powered pumpsets? Stuck with a three phase pumpset and a single phase supply?
+G2V's Autonomous Solar Pumpsets mitigate all of the above and more.`,
+  image:"https://static.wixstatic.com/media/48348e_def755d48af0477fbf2b1b52dba246cb~mv2.jpg/v1/fill/w_420,h_420,al_c,lg_1,q_80/24V-Mini-Agricultural-Irrigation-Solar-W.webp"
+ },
+ {
+  title:"Solar Street Lights",
+  description:"G2V offers smart street lights which are motion sensored. Experience street lights that will absolutely cost you NO single rupee to your power bill, once you install with us. The long-life LEDs and the Solar modules outlast traditional, power-hungry street-lamps.",
+  image:"https://static.wixstatic.com/media/48348e_1b6443cc8a754095ad6a744ae6b9ebdb~mv2.jpg/v1/fill/w_575,h_388,al_c,lg_1,q_80/100w-120w-3__02466_1501647759_1280_edited.webp"
+ }
+]
+
+export default data;
